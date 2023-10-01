f3f902b0a100a9b592d53c66d87a0da3432c59d3
Marcos Vinícios <marcosvenicus2017@gmail.com>
2023-10-01 04:28:05 -0300

    inicialização do projeto

---
+++ partejs.js
@@ -0,0 +1,9 @@
+//função que tem como objetivo gerar um número aleatório dentro de um intervalo pré-determinado, ou seja, utilizando os dois parâmetros de entrada como um valor máximo e um valor mínimo.
+//o "math.random" retorna um número decimal aleatório no intervalo semiaberto 0 e 1, enquanto o "math.floor" arredonda para um número inteiro o resultado do math.random.
+const gerarnumero = (min, max) => Math.floor(math.random() * (max - min + 1))
+//função que tem como objetivo "guardar" o número inteiro aleatório gerado anteriormente na função "gerarnumero"
+const iniciarjogo = (min, max) => {
+    const numerodestino = randomInt(min,max);
+
+
+}
