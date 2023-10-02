2a1df5c0a6af6d58bbd686068c9b522c08ebb72b
Marcos Vinícios <marcosvenicus2017@gmail.com>
2023-10-02 05:47:02 -0300

    segunda versão do código com adição de funções que irão configurar o ambiente do jogo

--- partejs.js
+++ partejs.js
@@ -4,6 +4,10 @@ const gerarnumero = (min, max) => Math.floor(math.random() * (max - min + 1))
 //função que tem como objetivo "guardar" o número inteiro aleatório gerado anteriormente na função "gerarnumero"
 const iniciarjogo = (min, max) => {
     const numerodestino = randomInt(min,max);
-
+//função que irá configurar o ambiente do jogo, aceitando como parâmetro de entrada um número de tentativas, utilizando referências aos elementos HTML no documento que serâo utilizados para 
+//interagir com o jogador. Até o momento, só foram pensados o botão para verificar os palpites e o campo de entrada onde o usuário irá digitar os palpites.
+    const jogo = (numerodetentativas) => {
+        const guessButton = document.getElementById('check');
+        const guessInput = document.getElementById('guess');
 
 }
