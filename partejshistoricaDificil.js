6ce4626b1912244aba360eed5a11fc4fe491a3db
Gustavo-Rodrigues1 <145784930+Gustavo-Rodrigues1@users.noreply.github.com>
2023-10-15 09:32:59 -0300

    Add files via upload

---
+++ partejshistoricaDificil.js
@@ -0,0 +1,76 @@
+//constante para armazenar e agrupar as questões
+const dificeis = [
+  {
+    questao: `"O homem é, por natureza, um animal político."`,
+    nome: "Aristóteles",
+  },
+  { 
+    questao: `"Só sei que nada sei"`, 
+    nome: "Sócrates" 
+  },
+  {
+    questao: `"Eu só deixo o catete morto"`,
+    nome: "Getúlio Vargas",
+    apelido: "Vargas",
+  },
+  {
+    questao: `"Independência ou morte!"`,
+    nome: "Dom Pedro I",
+    apelido: "Dom Pedro",
+  },
+  {
+    questao: `"Tente mover o mundo, mas comece movendo a si mesmo."`,
+    nome: "Arístocles",
+    apelido: "Platão",
+  },
+  {
+    questao: `"Aquilo que se faz por amor está sempre além do bem e do mal."`,
+    nome: "Friedrich Nietzsche",
+    apelido: "Nietzsche",
+  },
+  {
+    questao: `"Uma vida não examinada não merece ser vivida."`,
+    nome: "Arístocles",
+    apelido: "Platão",
+  },
+  {
+    questao: `"Acredite em milagres, mas não dependa deles."`,
+    nome: "Immanuel Kant",
+    apelido: "Kant",
+  },
+]; 
+
+// Função para armazenar os parâmetros em uma constante e exibir as respectivas questões
+const questoesEscolhidas = questoesSelecionadas(
+  dificeis.length - 1,
+  (quantidadeQuestoes = 8)
+);
+  
+// Função para pegar os índices das questões selecionadas
+const pegarIndices = usarIndices(dificeis, questoesEscolhidas);
+
+// Funções para exibir as respostas das questões
+const exibir1 = verificar("output1", "inputq1");
+const exibir2 = verificar("output2", "inputq2");
+const exibir3 = verificar("output3", "inputq3");
+const exibir4 = verificar("output4", "inputq4");
+const exibir5 = verificar("output5", "inputq5");
+const exibir6 = verificar("output6", "inputq6");
+const exibir7 = verificar("output7", "inputq7");
+const exibir8 = verificar("output8", "inputq8");
+
+// Adiciona um evento de clique ao botão "Enviar Resposta"
+document.getElementById("enviarRespostaID").addEventListener("click", () => {
+  // Exibe as respostas das questões
+  exibir1(verificarResposta(pegarIndices, 1));
+  exibir2(verificarResposta(pegarIndices, 2));
+  exibir3(verificarResposta(pegarIndices, 3));
+  exibir4(verificarResposta(pegarIndices, 4));
+  exibir5(verificarResposta(pegarIndices, 5));
+  exibir6(verificarResposta(pegarIndices, 6));
+  exibir7(verificarResposta(pegarIndices, 7));
+  exibir8(verificarResposta(pegarIndices, 8));
+});
+
+// Define as questões iniciais
+definirQuestoes(dificeis, questoesEscolhidas);
---
+++ partejshistoricaFacil.js
@@ -0,0 +1,68 @@
+//constante para armazenar e agrupar as questões
+const faceis = [
+  {
+    questao: `"O homem é, por natureza, um animal político."`,
+    nome: "Aristóteles",
+  },
+  { 
+    questao: `"Só sei que nada sei"`, 
+    nome: "Sócrates" 
+  },
+  {
+    questao: `"Eu só deixo o catete morto"`,
+    nome: "Getúlio Vargas",
+    apelido: "Vargas",
+  },
+  {
+    questao: `"Independência ou morte!"`,
+    nome: "Dom Pedro I",
+    apelido: "Dom Pedro",
+  },
+  {
+    questao: `"Tente mover o mundo, mas comece movendo a si mesmo."`,
+    nome: "Arístocles",
+    apelido: "Platão",
+  },
+  {
+    questao: `"Aquilo que se faz por amor está sempre além do bem e do mal."`,
+    nome: "Friedrich Nietzsche",
+    apelido: "Nietzsche",
+  },
+  {
+    questao: `"Uma vida não examinada não merece ser vivida."`,
+    nome: "Arístocles",
+    apelido: "Platão",
+  },
+  {
+    questao: `"Acredite em milagres, mas não dependa deles."`,
+    nome: "Immanuel Kant",
+    apelido: "Kant",
+  },
+]; 
+
+// Função para armazenar os parâmetros em uma constante e exibir as respectivas questões
+const questoesEscolhidas = questoesSelecionadas(
+  faceis.length - 1,
+  (quantidadeQuestoes = 4)
+);
+  
+// Função para pegar os índices das questões selecionadas
+const pegarIndices = usarIndices(faceis, questoesEscolhidas);
+
+// Funções para exibir as respostas das questões
+const exibir1 = verificar("output1", "inputq1");
+const exibir2 = verificar("output2", "inputq2");
+const exibir3 = verificar("output3", "inputq3");
+const exibir4 = verificar("output4", "inputq4");
+
+// Adiciona um evento de clique ao botão "Enviar Resposta"
+document.getElementById("enviarRespostaID").addEventListener("click", () => {
+  // Exibe as respostas das questões
+  exibir1(verificarResposta(pegarIndices, 1));
+  exibir2(verificarResposta(pegarIndices, 2));
+  exibir3(verificarResposta(pegarIndices, 3));
+  exibir4(verificarResposta(pegarIndices, 4));
+});
+
+// Define as questões iniciais
+definirQuestoes(faceis, questoesEscolhidas);
